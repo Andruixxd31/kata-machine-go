9c7da24bf29e2f77d75129cd7482f9b5ae8096b7
Andres <andruix93@gmail.com>
2025-04-16 10:11:36 -0600

    feat: implments tests for binary search

--- main.go
+++ main.go
@@ -9,11 +9,5 @@ import (
 func main() {
 	array := []int{0, 2, 3, 6, 8, 19, 23}
 	index := src.BinarySearch(array, 5, 0, len(array))
-	index2 := src.BinarySearch(array, 0, 0, len(array))
-	index3 := src.BinarySearch(array, 23, 0, len(array))
-	index4 := src.BinarySearch(array, 6, 0, len(array))
 	fmt.Printf("Index is %d \n", index)
-	fmt.Printf("Index is %d \n", index2)
-	fmt.Printf("Index is %d \n", index3)
-	fmt.Printf("Index is %d \n", index4)
 }
