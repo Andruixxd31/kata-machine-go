61c143da2e8314c455fe6564d22f0f0a793f7c81
Andres <andruix93@gmail.com>
2025-07-01 22:16:45 -0600

    feat: implements test to append method

--- src/singly_linked_list/singly_linked_list_test.go
+++ src/singly_linked_list/singly_linked_list_test.go
@@ -1,6 +1,7 @@
 package singlylinkedlist_test
 
 import (
+	"reflect"
 	"testing"
 
 	singlylinkedlist "github.com/andruixxd31/kata-machine-go/src/singly_linked_list"
@@ -30,29 +31,65 @@ func BenchmarkSinglyLinkedListAppend(b *testing.B) {
 
 }
 
-// func TestSinglyLinkedListAppend(t *testing.T) {
-// 	tests := []struct {
-// 		name string
-// 		ll   singlylinkedlist.LinkedList
-// 		want singlylinkedlist.LinkedList
-// 	}{
-// 		{
-// 			name: "Append a single element",
-// 			ll:   singlylinkedlist.LinkedList{},
-// 			want: singlylinkedlist.LinkedList{
-// 				Head: &singlylinkedlist.Node{
-// 					Val:  1,
-// 					Next: nil,
-// 				},
-// 				Tail: &singlylinkedlist.Node{
-// 					Val:  1,
-// 					Next: nil,
-// 				},
-// 			},
-// 		},
-// 	}
-//
-// 	for _, tt := range tests {
-//
-// 	}
-// }
+func TestSinglyLinkedListAppend(t *testing.T) {
+	tests := []struct {
+		name       string
+		ll         *singlylinkedlist.LinkedList
+		want       *singlylinkedlist.LinkedList
+		valueToAdd int
+	}{
+		{
+			name: "Append to empty singly linked list",
+			ll:   &singlylinkedlist.LinkedList{},
+			want: &singlylinkedlist.LinkedList{
+				Length: 1,
+				Head: &singlylinkedlist.Node{
+					Val:  1,
+					Next: nil,
+				},
+				Tail: &singlylinkedlist.Node{
+					Val:  1,
+					Next: nil,
+				},
+			},
+			valueToAdd: 1,
+		},
+		{
+			name: "Append to singly linked list with elements",
+			ll: &singlylinkedlist.LinkedList{
+				Length: 1,
+				Head: &singlylinkedlist.Node{
+					Val:  1,
+					Next: nil,
+				},
+				Tail: &singlylinkedlist.Node{
+					Val:  1,
+					Next: nil,
+				},
+			},
+			want: &singlylinkedlist.LinkedList{
+				Length: 2,
+				Head: &singlylinkedlist.Node{
+					Val:  1,
+					Next: nil,
+				},
+				Tail: &singlylinkedlist.Node{
+					Val:  2,
+					Next: nil,
+				},
+			},
+			valueToAdd: 2,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.ll.Append(&singlylinkedlist.Node{Val: tt.valueToAdd})
+
+			if !reflect.DeepEqual(tt.ll, tt.want) {
+				t.Errorf("%#v got %+v - want %+v", tt.ll, tt.ll, tt.want)
+
+			}
+		})
+	}
+}
