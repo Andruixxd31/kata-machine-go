8aea28d2fa5c0d1f3ed9319d6958501ef33acd9e
Andres <andruix93@gmail.com>
2025-07-01 19:29:34 -0600

    feat: implements benchmarking for print method

--- src/singly_linked_list/singly_linked_list_test.go
+++ src/singly_linked_list/singly_linked_list_test.go
@@ -3,32 +3,56 @@ package singlylinkedlist_test
 import (
 	"testing"
 
-	singlylinkedlist "github.com/andruixxd31/kata-machine-go/src/singly_linke_list"
+	singlylinkedlist "github.com/andruixxd31/kata-machine-go/src/singly_linked_list"
 )
 
-func TestSinglyLinkedListAppend(t *testing.T) {
-	tests := []struct {
-		name string
-		ll   singlylinkedlist.LinkedList
-		want singlylinkedlist.LinkedList
-	}{
-		{
-			name: "Append a single element",
-			ll:   singlylinkedlist.LinkedList{},
-			want: singlylinkedlist.LinkedList{
-				Head: &singlylinkedlist.Node{
-					Val:  1,
-					Next: nil,
-				},
-				Tail: &singlylinkedlist.Node{
-					Val:  1,
-					Next: nil,
-				},
-			},
-		},
+// TODO: Add a benchark on print linked list and see if other case is aplicable
+func BenchmarkSinglyLinkedListAppend(b *testing.B) {
+
+	var ll singlylinkedlist.LinkedList
+	var node singlylinkedlist.Node
+	node.Val = 1
+	node4 := singlylinkedlist.Node{
+		Val: 4,
+	}
+	node2 := singlylinkedlist.Node{
+		Val: 2,
 	}
 
-	for _, tt := range tests {
+	ll.PrintLinkedList()
+	ll.Append(&node)
+	ll.Append(&node2)
+	ll.Append(&node4)
 
+	for b.Loop() {
+		ll.PrintLinkedList()
 	}
+
 }
+
+// func TestSinglyLinkedListAppend(t *testing.T) {
+// 	tests := []struct {
+// 		name string
+// 		ll   singlylinkedlist.LinkedList
+// 		want singlylinkedlist.LinkedList
+// 	}{
+// 		{
+// 			name: "Append a single element",
+// 			ll:   singlylinkedlist.LinkedList{},
+// 			want: singlylinkedlist.LinkedList{
+// 				Head: &singlylinkedlist.Node{
+// 					Val:  1,
+// 					Next: nil,
+// 				},
+// 				Tail: &singlylinkedlist.Node{
+// 					Val:  1,
+// 					Next: nil,
+// 				},
+// 			},
+// 		},
+// 	}
+//
+// 	for _, tt := range tests {
+//
+// 	}
+// }
