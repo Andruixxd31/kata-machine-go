e83dd94d0c957f39d886bfeccdb0830b606f3306
Andres <andruix93@gmail.com>
2025-08-27 18:21:53 -0600

    feat: implements test for find method

---
+++ src/bst/bst_test.go
@@ -0,0 +1,62 @@
+package bst_test
+
+import (
+	"testing"
+
+	"github.com/andruixxd31/kata-machine-go/src/bst"
+)
+
+func TestFind(t *testing.T) {
+	tests := []struct {
+		name string
+		node bst.Node
+		bst  *bst.BST
+		want bool
+	}{
+		{
+			name: "Node is present",
+			node: bst.Node{
+				Val: 1,
+			},
+			bst: &bst.BST{
+				Root: &bst.Node{
+					Val: 5,
+					Left: &bst.Node{
+						Val: 1,
+					},
+					Right: &bst.Node{
+						Val: 10,
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "Node is not present",
+			node: bst.Node{
+				Val: 1,
+			},
+			bst: &bst.BST{
+				Root: &bst.Node{
+					Val: 5,
+					Left: &bst.Node{
+						Val: 2,
+					},
+					Right: &bst.Node{
+						Val: 10,
+					},
+				},
+			},
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.bst.Find(tt.node)
+			if got != tt.want {
+				t.Errorf("Want %t - got %t", got, tt.want)
+			}
+		})
+	}
+}
